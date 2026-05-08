bc7e7437a8a7d196db4745be1ac30d846fbb9e1d
HK-Lens <haitham.kojar@gmail.com>
2026-05-09 01:49:32 +0200

    Refactor carData with updated models and brands

--- cars-data.js
+++ cars-data.js
@@ -1,73 +1,52 @@
-// cars-data.js
+// cars-data.js - قاعدة بيانات شاملة لماركات وموديلات السيارات العالمية
 export const carData = {
-    "Volvo": [
-        "140", "240", "740", "850", "940", "960", "S40", "S60", "S80", "S90", 
-        "V40", "V50", "V60", "V70", "V90", "XC40", "XC60", "XC70", "XC90", 
-        "C30", "C70", "EX30", "EX90", "P1800"
-    ],
+    // --- ماركات أوروبية (سويد، ألمانيا، إيطاليا، فرنسا) ---
+    "Volvo": ["S60", "S90", "V60", "V90", "XC40", "XC60", "XC90", "C40 Recharge", "EX30", "EX90"],
+    "Volkswagen": ["Golf", "ID.3", "ID.4", "ID.5", "ID.7", "Passat", "Polo", "Tiguan", "Touareg", "T-Roc", "Arteon"],
+    "BMW": ["1-serie", "2-serie", "3-serie", "4-serie", "5-serie", "7-serie", "X1", "X3", "X5", "X7", "i3", "i4", "iX", "M3", "M5"],
+    "Mercedes-Benz": ["A-Klass", "C-Klass", "E-Klass", "S-Klass", "CLA", "GLA", "GLC", "GLE", "GLS", "EQA", "EQE", "EQS", "V-Klass"],
+    "Audi": ["A1", "A3", "A4", "A6", "A8", "Q3", "Q5", "Q7", "Q8", "e-tron", "Q4 e-tron", "RS6", "TT"],
+    "Porsche": ["911", "Cayenne", "Macan", "Panamera", "Taycan", "718 Cayman"],
+    "Polestar": ["Polestar 1", "Polestar 2", "Polestar 3", "Polestar 4"],
+    "Saab": ["9-3", "9-5", "900", "9000"], // للسيارات المستعملة الكلاسيكية في السويد
+    "Peugeot": ["208", "308", "508", "2008", "3008", "5008"],
+    "Renault": ["Clio", "Zoe", "Megane", "Captur", "Scenic", "Austral"],
+    "Fiat": ["500", "500e", "Panda", "Tipo", "Ducato"],
+    "Alfa Romeo": ["Giulia", "Stelvio", "Tonale"],
+    "Ferrari": ["488", "812 Superfast", "SF90", "Roma", "Purosangue"],
+    "Lamborghini": ["Aventador", "Huracán", "Urus"],
 
-    "BMW": [
-        "1-serie", "2-serie", "3-serie", "4-serie", "5-serie", "6-serie", "7-serie", "8-serie", 
-        "X1", "X2", "X3", "X4", "X5", "X6", "X7", "Z3", "Z4", 
-        "i3", "i4", "i5", "i7", "iX", "M2", "M3", "M4", "M5"
-    ],
+    // --- ماركات آسيوية (اليابان، كوريا الجنوبية، الصين) ---
+    "Toyota": ["Auris", "Avensis", "Aygo", "Camry", "Corolla", "GR Yaris", "Hilux", "Land Cruiser", "Prius", "RAV4", "Yaris", "bZ4X"],
+    "Honda": ["Civic", "CR-V", "HR-V", "Jazz", "Honda e"],
+    "Nissan": ["Ariya", "Juke", "Leaf", "Micra", "Navara", "Qashqai", "X-Trail"],
+    "Mazda": ["Mazda2", "Mazda3", "Mazda6", "CX-30", "CX-5", "CX-60", "MX-30", "MX-5"],
+    "Lexus": ["ES", "IS", "NX", "RX", "UX", "RZ"],
+    "Subaru": ["Forester", "Outback", "Solterra", "XV"],
+    "Mitsubishi": ["ASX", "Eclipse Cross", "Outlander PHEV", "L200"],
+    "Hyundai": ["i10", "i20", "i30", "Ioniq 5", "Ioniq 6", "Kona", "Santa Fe", "Tucson"],
+    "Kia": ["Ceed", "EV6", "EV9", "Niro", "Picanto", "Sportage", "Sorento", "Stinger"],
+    "BYD": ["Atto 3", "Han", "Tang", "Dolphin", "Seal"],
+    "MG": ["MG4", "MG5", "ZS EV", "Marvel R"],
 
-    "Mercedes-Benz": [
-        "A-Klass", "B-Klass", "C-Klass", "E-Klass", "S-Klass", "G-Klass", 
-        "CLA", "CLS", "GLA", "GLB", "GLC", "GLE", "GLS", "SL", "SLK", 
-        "EQA", "EQB", "EQC", "EQE", "EQS", "V-Klass", "Sprinter", "190", "240D"
-    ],
+    // --- ماركات أمريكية ---
+    "Tesla": ["Model 3", "Model S", "Model X", "Model Y", "Cybertruck"],
+    "Ford": ["Fiesta", "Focus", "Mustang", "Mustang Mach-E", "Kuga", "Explorer", "F-150 Ranger"],
+    "Chevrolet": ["Camaro", "Corvette", "Bolt", "Silverado"],
+    "Jeep": ["Avenger", "Compass", "Grand Cherokee", "Wrangler"],
+    "Cadillac": ["Escalade", "Lyriq", "XT4"],
 
-    "Audi": [
-        "A1", "A3", "A4", "A5", "A6", "A7", "A8", 
-        "Q2", "Q3", "Q4 e-tron", "Q5", "Q7", "Q8", 
-        "TT", "R8", "e-tron", "RS3", "RS4", "RS5", "RS6", "RSQ8"
-    ],
+    // --- ماركات بريطانية ---
+    "Land Rover": ["Defender", "Discovery", "Range Rover", "Range Rover Evoque", "Range Rover Sport", "Velar"],
+    "Jaguar": ["F-Pace", "I-Pace", "F-Type", "XE", "XF"],
+    "Mini": ["Cooper", "Countryman", "Clubman"],
+    "Bentley": ["Bentayga", "Continental GT"],
+    "Rolls-Royce": ["Ghost", "Phantom", "Cullinan"],
 
-    "Toyota": [
-        "Aygo", "Yaris", "Corolla", "Camry", "Avensis", "Auris", "C-HR", 
-        "RAV4", "Land Cruiser", "Hilux", "Prius", "Supra", "Celica", "bZ4X"
-    ],
-
-    "Tesla": [
-        "Model S", "Model 3", "Model X", "Model Y", "Cybertruck", "Roadster"
-    ],
-
-    "Volkswagen": [
-        "Polo", "Golf", "Passat", "Tiguan", "Touareg", "Arteon", "T-Roc", "T-Cross", 
-        "ID.3", "ID.4", "ID.5", "ID.7", "ID.Buzz", "Beetle", "Scirocco", "Caddy", "Transporter"
-    ],
-
-    "Kia": [
-        "Picanto", "Rio", "Ceed", "Proceed", "Sportage", "Sorento", "Niro", 
-        "EV6", "EV9", "Stinger", "Stonic", "Soul"
-    ],
-
-    "Hyundai": [
-        "i10", "i20", "i30", "Ioniq", "IONIQ 5", "IONIQ 6", "Kona", "Tucson", "Santa Fe"
-    ],
-
-    "Ford": [
-        "Fiesta", "Focus", "Mondeo", "Mustang", "Mustang Mach-E", "Explorer", "Kuga", "Puma", "Ranger"
-    ],
-
-    "Skoda": [
-        "Fabia", "Scala", "Octavia", "Superb", "Kamiq", "Karoq", "Kodiaq", "Enyaq"
-    ],
-
-    "Porsche": [
-        "911", "718 Boxster", "718 Cayman", "Taycan", "Panamera", "Macan", "Cayenne"
-    ],
-
-    "Nissan": [
-        "Micra", "Leaf", "Juke", "Qashqai", "X-Trail", "Ariya", "GT-R", "370Z"
-    ],
-
-    "Peugeot": [
-        "208", "308", "508", "2008", "3008", "5008"
-    ],
-
-    "Renault": [
-        "Clio", "Megane", "Captur", "Zoe", "Austral", "Espace", "Kangoo"
-    ]
+    // --- ماركات أخرى وفاخرة ---
+    "Skoda": ["Enyaq", "Fabia", "Octavia", "Superb", "Kodiaq", "Karoq"],
+    "Seat": ["Ibiza", "Leon", "Ateca", "Tarraco"],
+    "Cupra": ["Born", "Formentor", "Leon"],
+    "Dacia": ["Duster", "Sandero", "Jogger", "Spring"],
+    "Suzuki": ["Swift", "Vitara", "Jimny", "S-Cross"]
 };
