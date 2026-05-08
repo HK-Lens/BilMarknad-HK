c7e3b02224c5535c7b0a73f7019bb41296d5717d
HK-Lens <haitham.kojar@gmail.com>
2026-05-08 13:55:09 +0200

    Update Firebase configuration and imports

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyDCsMbG5pT3y6MC6b05LEdFoByWM1nT7NY",
+    authDomain: "bilmarknad-hk.firebaseapp.com",
+    projectId: "bilmarknad-hk",
+    storageBucket: "bilmarknad-hk.firebasestorage.app",
+    appId: "1:284008879407:web:66bee60b51ec277e8dbdda"
 };
-export default firebaseConfig;
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const googleProvider = new GoogleAuthProvider();
