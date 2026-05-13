c3dccc6496d11bd181053eea1eeab87dc0e613ea
HK-Lens <haitham.kojar@gmail.com>
2026-05-13 22:00:22 +0200

    Update cars-data.js

--- cars-data.js
+++ cars-data.js
@@ -1,53 +1,592 @@
-// cars-data.js - قاعدة بيانات شاملة لماركات وموديلات السيارات
+// cars-data.js
+
 export const carData = {
-    // --- ماركات أوروبية ---
-    "Volvo": ["S60", "S90", "V60", "V90", "XC40", "XC60", "XC90", "C40 Recharge", "EX30", "EX90", "240", "740", "850", "V40", "V70"],
-    "Volkswagen": ["Golf", "ID.3", "ID.4", "ID.5", "ID.7", "Passat", "Polo", "Tiguan", "Touareg", "T-Roc", "Arteon", "Beetle", "Scirocco"],
-    "BMW": ["1-serie", "2-serie", "3-serie", "4-serie", "5-serie", "7-serie", "8-serie", "X1", "X3", "X5", "X6", "X7", "i3", "i4", "iX", "M3", "M5", "Z4"],
-    "Mercedes-Benz": ["A-Klass", "B-Klass", "C-Klass", "E-Klass", "S-Klass", "CLA", "CLS", "GLA", "GLC", "GLE", "GLS", "EQA", "EQE", "EQS", "V-Klass", "SL"],
-    "Audi": ["A1", "A3", "A4", "A5", "A6", "A7", "A8", "Q2", "Q3", "Q4 e-tron", "Q5", "Q7", "Q8", "e-tron", "RS6", "TT", "R8"],
-    "Porsche": ["911", "Cayenne", "Macan", "Panamera", "Taycan", "718 Cayman", "718 Boxster"],
-    "Polestar": ["Polestar 1", "Polestar 2", "Polestar 3", "Polestar 4"],
-    "Saab": ["9-3", "9-5", "900", "9000", "9-3X"],
-    "Peugeot": ["208", "308", "508", "2008", "3008", "5008", "Partner"],
-    "Renault": ["Clio", "Zoe", "Megane", "Captur", "Scenic", "Austral", "Kangoo"],
-    "Fiat": ["500", "500e", "Panda", "Tipo", "Ducato", "Doblo"],
-    "Alfa Romeo": ["Giulia", "Stelvio", "Tonale", "Giulietta"],
-    "Ferrari": ["488", "812 Superfast", "SF90", "Roma", "Purosangue", "F8"],
-    "Lamborghini": ["Aventador", "Huracán", "Urus"],
 
-    // --- ماركات آسيوية ---
-    "Toyota": ["Auris", "Avensis", "Aygo", "Camry", "Corolla", "GR Yaris", "Hilux", "Land Cruiser", "Prius", "RAV4", "Yaris", "bZ4X", "C-HR"],
-    "Honda": ["Civic", "CR-V", "HR-V", "Jazz", "Honda e", "Accord"],
-    "Nissan": ["Ariya", "Juke", "Leaf", "Micra", "Navara", "Qashqai", "X-Trail", "370Z"],
-    "Mazda": ["Mazda2", "Mazda3", "Mazda6", "CX-30", "CX-5", "CX-60", "MX-30", "MX-5"],
-    "Lexus": ["ES", "IS", "NX", "RX", "UX", "RZ", "LC"],
-    "Subaru": ["Forester", "Outback", "Solterra", "XV", "Impreza"],
-    "Mitsubishi": ["ASX", "Eclipse Cross", "Outlander PHEV", "L200", "Space Star"],
-    "Hyundai": ["i10", "i20", "i30", "Ioniq 5", "Ioniq 6", "Kona", "Santa Fe", "Tucson", "Bayon"],
-    "Kia": ["Ceed", "EV6", "EV9", "Niro", "Picanto", "Sportage", "Sorento", "Stinger", "Rio"],
-    "BYD": ["Atto 3", "Han", "Tang", "Dolphin", "Seal"],
-    "MG": ["MG4", "MG5", "ZS EV", "Marvel R", "ZS"],
+    // ========================================
+    // Swedish Brands
+    // ========================================
 
-    // --- ماركات أمريكية ---
-    "Tesla": ["Model 3", "Model S", "Model X", "Model Y", "Cybertruck"],
-    "Ford": ["Fiesta", "Focus", "Mustang", "Mustang Mach-E", "Kuga", "Explorer", "F-150", "Ranger", "Puma"],
-    "Chevrolet": ["Camaro", "Corvette", "Bolt", "Silverado", "Tahoe"],
-    "Jeep": ["Avenger", "Compass", "Grand Cherokee", "Wrangler", "Renegade"],
-    "Cadillac": ["Escalade", "Lyriq", "XT4", "CT5"],
+    "Volvo": [
+        "240",
+        "740",
+        "850",
+        "S40",
+        "S60",
+        "S80",
+        "S90",
+        "V40",
+        "V50",
+        "V60",
+        "V70",
+        "V90",
+        "XC40",
+        "XC60",
+        "XC70",
+        "XC90",
+        "C30",
+        "C40 Recharge",
+        "EX30",
+        "EX90"
+    ],
 
-    // --- ماركات بريطانية ---
-    "Land Rover": ["Defender", "Discovery", "Range Rover", "Range Rover Evoque", "Range Rover Sport", "Velar"],
-    "Jaguar": ["F-Pace", "I-Pace", "F-Type", "XE", "XF"],
-    "Mini": ["Cooper", "Countryman", "Clubman"],
-    "Bentley": ["Bentayga", "Continental GT", "Flying Spur"],
-    "Rolls-Royce": ["Ghost", "Phantom", "Cullinan"],
+    "Saab": [
+        "900",
+        "9000",
+        "9-3",
+        "9-3X",
+        "9-5"
+    ],
 
-    // --- ماركات أخرى ---
-    "Skoda": ["Enyaq", "Fabia", "Octavia", "Superb", "Kodiaq", "Karoq", "Kamiq"],
-    "Seat": ["Ibiza", "Leon", "Ateca", "Tarraco", "Arona"],
-    "Cupra": ["Born", "Formentor", "Leon", "Ateca"],
-    "Dacia": ["Duster", "Sandero", "Jogger", "Spring", "Logan"],
-    "Suzuki": ["Swift", "Vitara", "Jimny", "S-Cross", "Ignis"],
-    "Annat": ["Övrigt"]
+
+    // ========================================
+    // German Brands
+    // ========================================
+
+    "Volkswagen": [
+        "Amarok",
+        "Arteon",
+        "Beetle",
+        "Bora",
+        "Caddy",
+        "Crafter",
+        "Golf",
+        "ID.3",
+        "ID.4",
+        "ID.5",
+        "ID.7",
+        "Jetta",
+        "Passat",
+        "Polo",
+        "Scirocco",
+        "Sharan",
+        "T-Cross",
+        "T-Roc",
+        "Taigo",
+        "Tiguan",
+        "Touareg",
+        "Touran",
+        "Transporter",
+        "Up"
+    ],
+
+    "BMW": [
+        "1-serie",
+        "2-serie",
+        "3-serie",
+        "4-serie",
+        "5-serie",
+        "6-serie",
+        "7-serie",
+        "8-serie",
+        "i3",
+        "i4",
+        "i5",
+        "i7",
+        "i8",
+        "iX",
+        "M2",
+        "M3",
+        "M4",
+        "M5",
+        "X1",
+        "X2",
+        "X3",
+        "X4",
+        "X5",
+        "X6",
+        "X7",
+        "XM",
+        "Z4"
+    ],
+
+    "Mercedes-Benz": [
+        "A-Klass",
+        "AMG GT",
+        "B-Klass",
+        "C-Klass",
+        "CLA",
+        "CLS",
+        "E-Klass",
+        "EQA",
+        "EQB",
+        "EQC",
+        "EQE",
+        "EQS",
+        "G-Klass",
+        "GLA",
+        "GLB",
+        "GLC",
+        "GLE",
+        "GLS",
+        "S-Klass",
+        "SL",
+        "Sprinter",
+        "V-Klass"
+    ],
+
+    "Audi": [
+        "A1",
+        "A3",
+        "A4",
+        "A5",
+        "A6",
+        "A7",
+        "A8",
+        "e-tron",
+        "Q2",
+        "Q3",
+        "Q4 e-tron",
+        "Q5",
+        "Q7",
+        "Q8",
+        "R8",
+        "RS3",
+        "RS4",
+        "RS6",
+        "TT"
+    ],
+
+    "Porsche": [
+        "718 Boxster",
+        "718 Cayman",
+        "911",
+        "Cayenne",
+        "Macan",
+        "Panamera",
+        "Taycan"
+    ],
+
+    "Opel": [
+        "Adam",
+        "Astra",
+        "Corsa",
+        "Crossland",
+        "Grandland",
+        "Insignia",
+        "Mokka",
+        "Vivaro"
+    ],
+
+    "Smart": [
+        "ForFour",
+        "ForTwo",
+        "#1",
+        "#3"
+    ],
+
+
+    // ========================================
+    // French Brands
+    // ========================================
+
+    "Peugeot": [
+        "108",
+        "208",
+        "308",
+        "408",
+        "508",
+        "2008",
+        "3008",
+        "5008",
+        "Boxer",
+        "Partner"
+    ],
+
+    "Renault": [
+        "Austral",
+        "Captur",
+        "Clio",
+        "Espace",
+        "Kadjar",
+        "Kangoo",
+        "Megane",
+        "Scenic",
+        "Talisman",
+        "Zoe"
+    ],
+
+    "Citroën": [
+        "Berlingo",
+        "C1",
+        "C3",
+        "C4",
+        "C5 Aircross",
+        "DS3",
+        "Jumpy"
+    ],
+
+    "DS": [
+        "DS3",
+        "DS4",
+        "DS7"
+    ],
+
+
+    // ========================================
+    // Italian Brands
+    // ========================================
+
+    "Fiat": [
+        "500",
+        "500e",
+        "Doblo",
+        "Ducato",
+        "Panda",
+        "Tipo"
+    ],
+
+    "Alfa Romeo": [
+        "Giulia",
+        "Giulietta",
+        "Stelvio",
+        "Tonale"
+    ],
+
+    "Ferrari": [
+        "488",
+        "812 Superfast",
+        "F8",
+        "Purosangue",
+        "Roma",
+        "SF90"
+    ],
+
+    "Lamborghini": [
+        "Aventador",
+        "Huracán",
+        "Revuelto",
+        "Urus"
+    ],
+
+    "Maserati": [
+        "Ghibli",
+        "GranTurismo",
+        "Grecale",
+        "Levante",
+        "MC20"
+    ],
+
+
+    // ========================================
+    // Japanese Brands
+    // ========================================
+
+    "Toyota": [
+        "Auris",
+        "Avensis",
+        "Aygo",
+        "bZ4X",
+        "C-HR",
+        "Camry",
+        "Corolla",
+        "GR86",
+        "GR Supra",
+        "GR Yaris",
+        "Hilux",
+        "Land Cruiser",
+        "Prius",
+        "Proace",
+        "RAV4",
+        "Yaris"
+    ],
+
+    "Honda": [
+        "Accord",
+        "Civic",
+        "CR-V",
+        "e",
+        "HR-V",
+        "Jazz"
+    ],
+
+    "Nissan": [
+        "370Z",
+        "Ariya",
+        "Juke",
+        "Leaf",
+        "Micra",
+        "Navara",
+        "Qashqai",
+        "X-Trail"
+    ],
+
+    "Mazda": [
+        "CX-30",
+        "CX-5",
+        "CX-60",
+        "Mazda2",
+        "Mazda3",
+        "Mazda6",
+        "MX-30",
+        "MX-5"
+    ],
+
+    "Lexus": [
+        "ES",
+        "IS",
+        "LC",
+        "NX",
+        "RX",
+        "RZ",
+        "UX"
+    ],
+
+    "Subaru": [
+        "Forester",
+        "Impreza",
+        "Outback",
+        "Solterra",
+        "XV"
+    ],
+
+    "Mitsubishi": [
+        "ASX",
+        "Eclipse Cross",
+        "L200",
+        "Outlander PHEV",
+        "Space Star"
+    ],
+
+    "Suzuki": [
+        "Ignis",
+        "Jimny",
+        "S-Cross",
+        "Swift",
+        "Vitara"
+    ],
+
+
+    // ========================================
+    // Korean Brands
+    // ========================================
+
+    "Hyundai": [
+        "Bayon",
+        "i10",
+        "i20",
+        "i30",
+        "Ioniq 5",
+        "Ioniq 6",
+        "Kona",
+        "Santa Fe",
+        "Tucson"
+    ],
+
+    "Kia": [
+        "Ceed",
+        "EV6",
+        "EV9",
+        "Niro",
+        "Picanto",
+        "Rio",
+        "Sorento",
+        "Sportage",
+        "Stinger"
+    ],
+
+
+    // ========================================
+    // Chinese Brands
+    // ========================================
+
+    "BYD": [
+        "Atto 3",
+        "Dolphin",
+        "Han",
+        "Seal",
+        "Tang"
+    ],
+
+    "MG": [
+        "Marvel R",
+        "MG4",
+        "MG5",
+        "ZS",
+        "ZS EV"
+    ],
+
+    "XPENG": [
+        "G6",
+        "G9",
+        "P7"
+    ],
+
+    "NIO": [
+        "EL6",
+        "EL7",
+        "ET5",
+        "ET7"
+    ],
+
+
+    // ========================================
+    // American Brands
+    // ========================================
+
+    "Tesla": [
+        "Cybertruck",
+        "Model 3",
+        "Model S",
+        "Model X",
+        "Model Y"
+    ],
+
+    "Ford": [
+        "Explorer",
+        "F-150",
+        "Fiesta",
+        "Focus",
+        "Kuga",
+        "Mustang",
+        "Mustang Mach-E",
+        "Puma",
+        "Ranger"
+    ],
+
+    "Chevrolet": [
+        "Bolt",
+        "Camaro",
+        "Corvette",
+        "Silverado",
+        "Tahoe"
+    ],
+
+    "Jeep": [
+        "Avenger",
+        "Compass",
+        "Grand Cherokee",
+        "Renegade",
+        "Wrangler"
+    ],
+
+    "Cadillac": [
+        "CT5",
+        "Escalade",
+        "Lyriq",
+        "XT4"
+    ],
+
+    "Dodge": [
+        "Challenger",
+        "Charger",
+        "Durango",
+        "RAM"
+    ],
+
+
+    // ========================================
+    // British Brands
+    // ========================================
+
+    "Land Rover": [
+        "Defender",
+        "Discovery",
+        "Range Rover",
+        "Range Rover Evoque",
+        "Range Rover Sport",
+        "Velar"
+    ],
+
+    "Jaguar": [
+        "F-Pace",
+        "F-Type",
+        "I-Pace",
+        "XE",
+        "XF"
+    ],
+
+    "Mini": [
+        "Clubman",
+        "Cooper",
+        "Countryman"
+    ],
+
+    "Bentley": [
+        "Bentayga",
+        "Continental GT",
+        "Flying Spur"
+    ],
+
+    "Rolls-Royce": [
+        "Cullinan",
+        "Ghost",
+        "Phantom"
+    ],
+
+    "Aston Martin": [
+        "DB11",
+        "DB12",
+        "DBX",
+        "Vantage"
+    ],
+
+
+    // ========================================
+    // Other European Brands
+    // ========================================
+
+    "Skoda": [
+        "Enyaq",
+        "Fabia",
+        "Kamiq",
+        "Karoq",
+        "Kodiaq",
+        "Octavia",
+        "Superb"
+    ],
+
+    "Seat": [
+        "Arona",
+        "Ateca",
+        "Ibiza",
+        "Leon",
+        "Tarraco"
+    ],
+
+    "Cupra": [
+        "Ateca",
+        "Born",
+        "Formentor",
+        "Leon",
+        "Tavascan"
+    ],
+
+    "Dacia": [
+        "Duster",
+        "Jogger",
+        "Logan",
+        "Sandero",
+        "Spring"
+    ],
+
+    "Polestar": [
+        "Polestar 1",
+        "Polestar 2",
+        "Polestar 3",
+        "Polestar 4"
+    ],
+
+
+    // ========================================
+    // Electric / New Brands
+    // ========================================
+
+    "Lucid": [
+        "Air"
+    ],
+
+    "Rivian": [
+        "R1S",
+        "R1T"
+    ],
+
+
+    // ========================================
+    // Fallback
+    // ========================================
+
+    "Annat": [
+        "Övrigt"
+    ]
+
 };
