6b32def07c75ce0aa31f5517ca0df917daba2b3c
HK-Lens <haitham.kojar@gmail.com>
2026-05-14 15:28:56 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDCsMbG5pT3y6MC6b05LEdFoByWM1nT7NY",
+    authDomain: "bilmarknad-hk.firebaseapp.com",
+    projectId: "bilmarknad-hk",
+    storageBucket: "bilmarknad-hk.firebasestorage.app",
+    appId: "1:284008879407:web:66bee60b51ec277e8dbdda"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const googleProvider = new GoogleAuthProvider();
